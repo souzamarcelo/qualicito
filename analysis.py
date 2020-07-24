7c96797b20007e42f6ef8c441bad7334052b7d18
Marcelo de Souza <marcelo.desouza@udesc.br>
2020-07-24 15:57:53 -0300

    New tables; modifications in classes.

--- analysis.py
+++ analysis.py
@@ -23,38 +23,50 @@ def leituraDados(estados, anos, filtroExames, limitesClasses):
             dadosAnoEstado['per_hsil'] = dadosAnoEstado['hsil'] / dadosAnoEstado['ex_sat'] * 100
             dadosAnoEstado['per_ins'] = dadosAnoEstado['ex_ins'] / dadosAnoEstado['ex_total'] * 100
             dados = pd.concat([dados, dadosAnoEstado], ignore_index = True)
-
+    
     # Filtro
     dados = dados[dados['ex_total'] >= filtroExames]
     
+    # Classes para labs
+    dados['classe'] = dados['ex_total'].map(lambda x: 'pouco' if x < limitesClasses[0] else 'razoavel' if x < limitesClasses[1] else 'bom' if x < limitesClasses[2] else 'ideal')
+
     # Identificador para labs
     listaLabs = list(dados['lab_desc'].unique())
     dados['lab'] = dados['lab_desc'].map(lambda x: listaLabs.index(x) + 1)
     dados = dados.reset_index()
-
-    # Classes para labs
-    listaLabs = list(dados['lab'].unique())
-    for lab in listaLabs:
-        minExames = dados[dados['lab'] == lab]['ex_total'].min()
-        dados.loc[dados['lab'] == lab, 'classe'] = 'pouco' if minExames < limitesClasses[0] else 'razoavel' if minExames < limitesClasses[1] else 'bom' if minExames < limitesClasses[2] else 'ideal'
+    
+    # Remove inf
+    dados.loc[dados['sil_total'] == 0, 'raz_asc_sil'] = float('NaN')
 
     return dados
 
 
-def laboratoriosEstadoClasse(dados):
-    d = dados[dados['ano'] == 2019].groupby(['estado', 'classe']).agg({'lab': 'count'})
-    d = d.unstack('classe')
+def quantidadeLaboratorios(dados):
+    d = dados
+    d['ano'] = d['ano'].map(lambda x: str(x))
+    d = d.groupby(['estado', 'classe', 'ano']).agg({'lab': 'count'})
+    d = d.unstack('ano')
     d.columns = d.columns.droplevel()
     d.columns.name = ''
     d.reset_index(inplace = True)
+    totais = d.groupby(['estado']).agg({'2015': 'sum', '2016': 'sum', '2017': 'sum', '2018': 'sum', '2019': 'sum', 'classe': lambda x: 'TOTAL'})
+    totais.reset_index(inplace = True)
+    d = d.append(totais)
     d.estado = pd.Categorical(d.estado, categories = ['rs', 'sc', 'pr'], ordered = True)
-    d.sort_values('estado', inplace = True, ignore_index = True)
-    d = d[['estado', 'pouco', 'razoavel', 'bom', 'ideal']]
-    d['total'] = d['pouco'] + d['razoavel'] + d['bom'] + d['ideal']
+    d.classe = pd.Categorical(d.classe, categories = ['pouco', 'razoavel', 'bom', 'ideal', 'TOTAL'], ordered = True)
+    d.sort_values(['estado', 'classe'], inplace = True, ignore_index = True)
+
+    for estado in d['estado'].unique():
+        for ano in [2015, 2016, 2017, 2018, 2019]:
+            total = d[(d['estado'] == estado) & (d['classe'] != 'TOTAL')][str(ano)].sum()
+            for classe in d['classe'].unique():
+                value = float(d[(d['estado'] == estado) & (d['classe'] == classe)][str(ano)])
+                d.loc[(d['estado'] == estado) & (d['classe'] == classe), str(ano)] = str(int(value)) + ' (' + str(round(value / total * 100, 1)) + '%)'
+
     return d
 
 
-def examesEstadoClasseAno(dados):
+def quantidadeExames(dados):
     d = dados
     d['ano'] = d['ano'].map(lambda x: str(x))
     d = d.groupby(['estado', 'classe', 'ano']).agg({'ex_total': 'sum'})
@@ -72,8 +84,26 @@ def examesEstadoClasseAno(dados):
     return d
 
 
+def tabelaIndicador(dados, indicador, medida):
+    d = dados[['lab', 'ano', 'estado', 'classe', indicador]]
+    d = d.groupby(['estado', 'classe', 'ano']).agg({indicador: medida})
+    d = d.unstack('ano')
+    d.columns = d.columns.droplevel()
+    d.columns.name = ''
+    d.reset_index(inplace = True)
+    d.estado = pd.Categorical(d.estado, categories = ['rs', 'sc', 'pr'], ordered = True)
+    d.classe = pd.Categorical(d.classe, categories = ['pouco', 'razoavel', 'bom', 'ideal'], ordered = True)
+    d.sort_values(['estado', 'classe'], inplace = True, ignore_index = True)
+    return d
+
+
+
+
+# Rotinas de teste
 estados = ['rs', 'sc', 'pr']
 anos = [2015, 2016, 2017, 2018, 2019]
 m = 1500
 v = [5000, 10000, 15000] #v1, v2, v3
 dados = leituraDados(estados, anos, m, v)
+#print(tabelaIndicador(dados, 'per_asc_alt', 'mean'))
+#print(tabelaIndicador(dados, 'ind_pos', 'mean'))
